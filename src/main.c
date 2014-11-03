f8d5f89a3dcee597498218ae0c026e5c4f9361bf
Blanboom <i@blanboom.org>
2014-11-03 16:04:48 +0800

    添加 MPU6050 驱动

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 #include "diag/Trace.h"
 #include "stm32f10x.h"
 #include "aMatrix_conf.h"
+#include "MPU6050.h"
 
 
 
@@ -16,6 +17,10 @@ int main(int argc, char* argv[])
 {
 	trace_printf("aMatrix v%s\n", version);
     trace_printf("System clock: %uHz\n", SystemCoreClock);
+
+    MPU6050_I2C_Init();
+    MPU6050_Initialize();  trace_printf("MPU6050 Init: %d\n", MPU6050_TestConnection());
+
 
 
 
