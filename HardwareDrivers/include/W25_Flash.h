844c2971482b89642d1984eb901cec6390072831
Blanboom <i@blanboom.org>
2014-11-16 15:27:35 +0800

    完成 W25Q128 Flash 驱动

---
+++ HardwareDrivers/include/W25_Flash.h
@@ -0,0 +1,40 @@
+#ifndef __SPI_FLASH_H
+#define __SPI_FLASH_H
+
+#include "stm32f10x.h"
+
+#define SPI_FLASH_SPI                           SPI1
+#define SPI_FLASH_SPI_CLK                       RCC_APB2Periph_SPI1
+#define SPI_FLASH_PORT                          GPIOA
+#define SPI_FLASH_GPIO_CLK                      RCC_APB2Periph_GPIOA
+#define SPI_FLASH_SPI_SCK_PIN                   GPIO_Pin_5                  /* PA.05 */
+#define SPI_FLASH_SPI_MISO_PIN                  GPIO_Pin_6                  /* PA.06 */
+#define SPI_FLASH_SPI_MOSI_PIN                  GPIO_Pin_7                  /* PA.07 */
+#define SPI_FLASH_CS_PIN                        GPIO_Pin_4                  /* PA.04 */
+
+
+#define SPI_FLASH_CS_LOW()       GPIO_ResetBits(SPI_FLASH_PORT, SPI_FLASH_CS_PIN)
+#define SPI_FLASH_CS_HIGH()      GPIO_SetBits(SPI_FLASH_PORT, SPI_FLASH_CS_PIN)
+
+
+void SPI_FLASH_Init(void);
+void SPI_FLASH_SectorErase(u32 SectorAddr);
+void SPI_FLASH_BulkErase(void);
+void SPI_FLASH_PageWrite(u8* pBuffer, u32 WriteAddr, u16 NumByteToWrite);
+void SPI_FLASH_BufferWrite(u8* pBuffer, u32 WriteAddr, u16 NumByteToWrite);
+void SPI_FLASH_BufferRead(u8* pBuffer, u32 ReadAddr, u16 NumByteToRead);
+u32 SPI_FLASH_ReadID(void);
+u32 SPI_FLASH_ReadDeviceID(void);
+void SPI_FLASH_StartReadSequence(u32 ReadAddr);
+void SPI_Flash_PowerDown(void);
+void SPI_Flash_WAKEUP(void);
+
+
+u8 SPI_FLASH_ReadByte(void);
+u8 SPI_FLASH_SendByte(u8 byte);
+u16 SPI_FLASH_SendHalfWord(u16 HalfWord);
+void SPI_FLASH_WriteEnable(void);
+void SPI_FLASH_WaitForWriteEnd(void);
+
+#endif /* __SPI_FLASH_H */
+
--- src/main.c
+++ src/main.c
@@ -1,11 +1,7 @@
-
-
 #include <stdio.h>
 #include "diag/Trace.h"
 #include "stm32f10x.h"
 #include "aMatrix_conf.h"
-#include "MPU6050.h"
-
 
 
 #pragma GCC diagnostic push
@@ -18,15 +14,10 @@ int main(int argc, char* argv[])
 	trace_printf("aMatrix v%s\n", version);
     trace_printf("System clock: %uHz\n", SystemCoreClock);
 
-    MPU6050_I2C_Init();
-    MPU6050_Initialize();  trace_printf("MPU6050 Init: %d\n", MPU6050_TestConnection());
+	while (1)
+	{
 
-
-
-
-    while (1)
-    {
-    }
+	}
 }
 
 #pragma GCC diagnostic pop
